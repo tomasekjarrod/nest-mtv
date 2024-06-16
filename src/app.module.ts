55070a3b6e98fe10e5c8e16803e48c3efe4fe654
Jarrod <jtomasek@stedwards.edu>
2024-06-16 11:10:18 -0500

    typeorm installed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT || ''),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: ['**/*.entity{.ts,.js}'],
+      migrationsTableName: 'migration',
+      migrations: ['src/migration/*.ts'],
+      synchronize: true, // Set this to false in production
+    }),
+    AuthModule,
+  ],
   controllers: [AuthController],
 })
 export class AppModule {}
