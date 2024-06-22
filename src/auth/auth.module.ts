bc9bdf42fcb1ed719ff4a738ae01ff599074a92b
Jarrod <jtomasek@stedwards.edu>
2024-06-22 13:06:22 -0500

    generating jwt on login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '2 days' },
+    }),
+    UsersModule,
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { IUser, UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+  ) {}
 
   async signup(createUserDto: CreateUserDto): Promise<any> {
-    const user = await this.usersService.create(createUserDto);
+    const { id } = await this.usersService.create(createUserDto);
 
-    // TODO: JWT here
-    return user;
+    const access_token = await this.jwtService.signAsync({ sub: id });
+
+    return { access_token };
   }
 }
